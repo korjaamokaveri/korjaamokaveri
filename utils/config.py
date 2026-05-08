cbce656ad8d010dee0408eb6aacd86e441a1a69f
korjaamokaveri <ville_salovaara@hotmail.com>
2026-05-08 23:30:35 +0300

    Delete Config class from config.py
    Remove redundant Config class and its attributes.

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,1 @@
-import os
 
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "vaihda-tama-myöhemmin-oikeasti-salaiseksi-avaimeksi")
-    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-    UPLOAD_FOLDER = "static/uploads/tickets"
-    DB_PATH = os.environ.get("DB_PATH", "korjaamo_kaveri.db")
-    DEBUG = os.environ.get("FLASK_DEBUG", "True") == "True"
-    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-    UPLOAD_FOLDER = "static/uploads/tickets"
