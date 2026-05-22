8e030d26290e902560fc92c91fbec878d8c58200
korjaamokaveri <ville_salovaara@hotmail.com>
2026-05-22 22:59:03 +0300

    Update fault_code_service.py

--- services/fault_code_service.py
+++ services/fault_code_service.py
@@ -336,11 +336,11 @@ def delete_fault_code(fault_code_id: int):
     conn = get_connection()
     cur = conn.cursor()
 
+    cur.execute("DELETE FROM fault_category_suggestions WHERE fault_code_id = ?", (fault_code_id,))
     cur.execute("DELETE FROM possible_causes WHERE fault_code_id = ?", (fault_code_id,))
     cur.execute("DELETE FROM symptom_matches WHERE fault_code_id = ?", (fault_code_id,))
     cur.execute("DELETE FROM test_steps WHERE fault_code_id = ?", (fault_code_id,))
     cur.execute("DELETE FROM vehicle_rules WHERE fault_code_id = ?", (fault_code_id,))
-    cur.execute("DELETE FROM suggested_fault_code_updates WHERE fault_code_id = ?", (fault_code_id,))
     cur.execute("DELETE FROM fault_codes WHERE id = ?", (fault_code_id,))
 
     conn.commit()
