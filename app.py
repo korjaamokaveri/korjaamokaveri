c1db5bd85056d9992f0c0f9c3aef6caeb69a7acd
korjaamokaveri <ville_salovaara@hotmail.com>
2026-05-23 00:13:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,14 @@ def create_app():
     app.config.from_object(Config)
     app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024
 
+    app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+    app.config["MAIL_USE_TLS"] = True
+
+    mail.init_app(app)
+
     app.register_blueprint(admin_repair_guides_bp)
     app.register_blueprint(admin_bp)
 
